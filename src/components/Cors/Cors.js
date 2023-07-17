73b8343c1c8e44611ed7908ca486acbf0fe44395
Vgrotherz <vgrotherz@gmail.com>
2023-07-17 22:57:26 +0700

    some tech changes

--- src/components/Cors/Cors.js
+++ src/components/Cors/Cors.js
@@ -3,13 +3,13 @@ import "./Cors.css";
 
 const Cors = () => {
     const handleButtonClick = () => {
-      window.open('https://cors-anywhere.herokuapp.com/corsdemo', '_blank', 'width=600,height=400');
+      window.open('https://cors-anywhere.herokuapp.com/corsdemo', '_blank', 'width=600,height=200');
     };
   
     return (
         <div className="corsBtn">
             <button onClick={handleButtonClick}>
-                Go to another site
+                Click me first!
             </button>
       </div>
     );
