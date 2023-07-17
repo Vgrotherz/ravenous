3c314742421ee013fa795761c3cbb9b666e9e3d1
Vgrotherz <vgrotherz@gmail.com>
2023-07-17 20:34:00 +0700

    click me first btn open in a small window

--- src/components/Cors/Cors.js
+++ src/components/Cors/Cors.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "./Cors.css";
 
 const Cors = () => {
+    const handleButtonClick = () => {
+      window.open('https://cors-anywhere.herokuapp.com/corsdemo', '_blank', 'width=600,height=400');
+    };
+  
     return (
-        <div>
-           <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="popup" onclick="window.open('https://cors-anywhere.herokuapp.com/corsdemo','name','width=600,height=400')">Open page in new window</a>
-        </div>
+        <div className="corsBtn">
+            <button onClick={handleButtonClick}>
+                Go to another site
+            </button>
+      </div>
     );
-}
+  };
+
 
 export default Cors;
